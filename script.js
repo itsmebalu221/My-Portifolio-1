7722e08bf20135a3cfa6258183c9eb4a8959dc9f
itsmebalu221 <154417002+itsmebalu221@users.noreply.github.com>
2024-07-08 12:19:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.querySelector("#like1").addEventListener("click", () => {
+    document.querySelector("#like1").style.backgroundColor = "#234846";
+    document.querySelector(".svg1").style.color = "white";
+});
+
+document.querySelector("#like2").addEventListener("click", () => {
+    document.querySelector("#like2").style.backgroundColor = "#234846";
+    document.querySelector(".svg2").style.color = "white";
+});
+
+document.querySelector("#like3").addEventListener("click", () => {
+    document.querySelector("#like3").style.backgroundColor = "#234846";
+    document.querySelector(".svg3").style.color = "white";
+});
+
+document.querySelector("#like4").addEventListener("click", () => {
+    document.querySelector("#like4").style.backgroundColor = "#234846";
+    document.querySelector(".svg4").style.color = "white";
+});
+
+document.querySelector("#anna-kshetra").addEventListener("click",()=>{
+    alert("Website currently unavailable. Sorry for the inconvenience.")
+})
+
